3d0dae61ff813a45756e231decd6f3c0e44d98f2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-12-03 08:33:33 -0500

    Added card template. Added search. Images.
    Added card template for the initial recipe images/cards. Clicking a card
    will result in a second window popping up that has the full sized image
    as well as the actual recipe in it.
    
    Search by title works nicely. Fades recipes in and out if they match the
    criteria or not. A full text search on the recipe is possible, but lets
    not do that yet. Too much would pop up for 'bak' I bet. Or 'heat' and
    other general cooking terms. So title search will do for now.
    
    Added some styling to force img's in cards to be a specific size. I
    wonder if there's a way to fix it without using a background image?

--- application.js
+++ application.js
@@ -1,24 +1,75 @@
 jQuery( document ).ready(function( $ ) {
 	var template = null;
+	var cardTemplate = null;
 	$('#recipe-template').load("recipe.html",function(html){ 
 		template = Handlebars.compile(html);
+		cardTemplate = Handlebars.compile($('#card-template').html());
 
 		$('#recipe-data').load("data/recipes.js",function(raw){
 			var data = JSON.parse(raw);
 			for (var i = data.length - 1; i >= 0; i--) {
-				console.log(data[i]);
-				var recipeHtml = template(data[i]);
-				$(recipeHtml).find(".directions p").hide();
+				data[i].index = i;
+				var recipeHtml = cardTemplate(data[i]);
 				$('#content').append( recipeHtml.replace('data-src','src') );
-
 			};
-		});
 
-		$('#content').on('click', '.directions', function(evt){
-				$(this).find("p").slideToggle();
-		});
+			/* Clicking the card will show the recipe */ 
+			$('#content').on('click', '.card-click', function(evt){
+				var index = $(this).find('.card-title').attr('ref');
+				var recipeHtml = template(data[index]);
+				$('#recipe-display').html( recipeHtml.replace('data-src','src') );
+				$('#recipe-display').show();
+				
+				evt.stopPropagation();
+				console.log('1st');
+			});
 
-		$('#loader').hide();
+			/* Only hide the loader if we loaded everything */
+			$('#content').fadeIn();
+			$('#loader').fadeOut();
+		});
+		
 		return false;
 	});
+
+	/* Search functionality for recipes based on title only.*/
+	$('#search').val($('#search').attr('title'));
+	$('#search').on('keyup',function(evt){
+		var val = $(this).val().toUpperCase();
+		var titles = $('.card-title');
+		for (var i = titles.length - 1; i >= 0; i--) {
+			var item = $(titles[i]);
+			if(item.text()){ /*template has no value*/
+				if( item.text().toUpperCase().search(val) == -1)
+					item.closest('.card-container').fadeOut();	
+				else
+					item.closest('.card-container').fadeIn();	
+			}
+		};
+		if(val.trim().length <= 0)
+			titles.parent().parent().fadeIn();
+	});
+	$('#search').on('focus',function(evt){
+		if($(this).val() == $(this).attr('title')){
+			$(this).val('');
+		}
+	});
+	$('#search').on('blur',function(evt){
+		var val = $(this).val();
+		if(val.trim().length <= 0 || val == $(this).attr('title'))
+			$('.recipe-title').parent().parent().fadeIn();
+		$(this).val($(this).attr('title'));
+	});
+
+	/* Closing of Dialogs if clicked out */
+	$('#recipe-display').click(function(evt){evt.stopPropagation();});
+	$(document).click(function(event) { 
+	    if($(event.target).parents().index($('#recipe-display')) == -1) {
+	        if($('#recipe-display').is(":visible")) {
+	            $('#recipe-display').hide()
+	            
+	        }
+	    }        
+	});
+
 });
--- data/recipes.js
+++ data/recipes.js
@@ -1,22 +1,68 @@
 [
 {
 	"title" : "Fried Chicken",
-	"image" : "ajax-loader.gif",
+	"image" : "fried-chicken.jpg",
 	"difficulty" : "Easy",
 	"ingredients" : [
-		{"name" : "chicken"},{ "name" : "panko"},{ "name" : "oil"},{ "name" : "1 egg"}
+		{"name" : "chicken"},{ "name" : "breadcrumbs"},{ "name" : "oil"},{ "name" : "1 egg"}
 	],
 	"excerpt" : "Easy to make fried Chicken",
-	"directions" : "Do this and this and this"
+	"directions" : [
+		{"instruction" : "Turn a frying pan to medium heat and pour enough oil to cover the bottom of the pan in."},
+		{"instruction" : "While you're waiting for the oil to heat up, pour the breadcrumbs into a bowl and beat an egg in other."},
+		{"instruction" : "Dip the chicken in the egg and then roll it into the breakcrumbs."},
+		{"instruction" : "Put each piece of breaded chicken aside until the oil in the pan is hot."},
+		{"instruction" : "Lay the chicken into the oil and turn each piece over every few minutes until the chicken is cooked well."},
+		{"instruction" : "Serve hot with whatever sauces or spices you'd like."}
+	]
 },
 {
-	"title" : "Cheddar Chicken",
-	"image" : "ajax-loader.gif",
-	"difficulty" : "Easy",
+	"title" : "Baked Cheddar Chicken",
+	"image" : "baked-chedder-chicken.jpg",
+	"difficulty" : "Medium",
 	"ingredients" : [
-		{"name" : "chicken"},{ "name" : "shredded cheese"},{ "name" : "milk"},{ "name" : "1 egg"},{"name" : "breadcrumbs"}
+		{"name" : "Chicken Breast"},{ "name" : "Shredded cheese"},{ "name" : "Milk"},{ "name" : "Melted Butter"},{"name" : "Breadcrumbs"}
 	],
 	"excerpt" : "Simple baked chicken recipe",
-	"directions" : "Do this and this and this"
+	"directions" : [
+		{"instruction" : "Oven to 400 degrees"},
+		{"instruction" : "Dip chicken in milk, cheese, then breadcrumbs"},
+		{"instruction" : "Place on pan, cover in foil"},
+		{"instruction" : "Bake for 25 minutes"},
+		{"instruction" : "Brush butter on and cook another 10 or until golden brown"},
+		{"instruction" : "Serve with noodles, sauce, and spices to your liking"}
+	]
+},
+{
+	"title" : "Apple Kiwi Pie",
+	"image" : "kiwi-apple-pie.jpg",
+	"difficulty" : "Medium",
+	"ingredients" : [
+		{"name" : "1/3 Cup Sugar"}, 
+		{"name" : "1/3 cup brown sugar"},
+		{"name" : "2.5 tablespoon flour"},
+		{"name" : "1 teaspoon cinnamin"},
+		{"name" : "1/4 cup nutmeg"},
+		{"name" : "5 Sliced Kiwis"},
+		{"name" : "3 Sliced Apples"},
+		{"name" : "1 tablespoon lemon juice"},
+		{"name" : "1 Pie Crust"},
+		{"name" : "stick of butter"}
+	],
+	"excerpt" : "Tasty and Sweet pie",
+	"directions" : [
+		{"instruction" : "Preheat oven to 375 degrees F"},
+		{"instruction" : "Mix sugars, flour, cinnamin, and nutmeg in a bowl"},
+		{"instruction" : "Add kiwis, apples, and lemon juice to bowl"},
+		{"instruction" : "Toss and mix bowl and let stand for 15 minutes"},
+		{"instruction" : "While waiting, roll out and line pie pan with crust"},
+		{"instruction" : "Use finger and thumb to make artistic bumps along the edge"},
+		{"instruction" : "Pour fruit mixture into pie crust"},
+		{"instruction" : "Slice butter thinly and place evenly spaced on top of the fruit mixture"},
+		{"instruction" : "Using and excess pie edge, squish and mix with a dash of brown sugar to make a sweet crumble"},
+		{"instruction" : "sprinkle top of pie with crumble or seal with a second pie crust if you have one (slice to vent)"},
+		{"instruction" : "Cover edge of pie with tin foil"},
+		{"instruction" : "Bake pie in center of oven for 20 minutes, remove strip bake for 25-30 more"}
+	]
 }
 ]
